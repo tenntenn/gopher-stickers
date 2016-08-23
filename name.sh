f68dbed7dc51e29231b1e71937e2353a391ec2bf
Takuya Ueda <uedatakuya@gmail.com>
2016-08-23 16:32:29 +0900

    add new stickers and script

---
+++ name.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+LIST=(
+    "hi"
+    "cry"
+    "angry"
+    "cheer"
+    "hide"
+    "hide_away"
+    "sigh"
+    "hungry"
+    "beer"
+    "sleepy"
+    "sleeping"
+    "awake"
+    "embarrass"
+    "ok"
+    "no"
+    "good_morning"
+    "hot"
+    "cold"
+    "work"
+    "lovely"
+    "question"
+    "surprise"
+    "tehepero"
+    "bye"
+    "thank_you"
+    "hot_spring"
+    "scare"
+    "faint"
+    "sorry"
+    "run_away"
+    "cook"
+    "baseball"
+    "balloon"
+    "autumn"
+    "spring"
+    "ninja"
+)
+
+for (( I = 0; I < ${#LIST[@]}; ++I ))
+do
+    mv png/$(printf "%02d" $(expr $I + 1)).png png/${LIST[$I]}.png
+done
